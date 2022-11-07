0a8e7f51a7b4e56fd726ea082cec4da6f1cdf4a9
Ivy011 <115562322+Ivy011@users.noreply.github.com>
2022-11-07 19:02:46 +0800

    week10 class exericse

--- entry3/Color.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('color').addEventListener('input', function (event) {
-    document.getElementById('container').style.backgroundColor = event.target.value;
-})
-
---
+++ jquery demo/jquery.js
@@ -0,0 +1,6 @@
+// try playing with this ALERT - 
+// $(document).ready(function() {alert("[hello earthlings]"); })
+
+// JS files comment with two backslash lines 
+$(document).ready(function() {$("#cat").draggable(); })
+$(document).ready(function() {$("#1").draggable(); })
